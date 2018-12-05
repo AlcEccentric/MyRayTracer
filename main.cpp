3112c3d21a5c89c487cbc4644bd807c773a03b2f
AlcEccentric <lihongyide@gmail.com>
2018-12-05 23:11:03 +0800

    render a simple sphere

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r){
+    float a = r.dir().squared_length();
+    float b = dot(r.ori() - center, r.dir()) * 2;
+    float c = (r.ori() - center).squared_length() - radius * radius;
+    return (b*b - 4*a*c > 0);
+}
 vec3 color(const ray& r){
     // generating a color according the ray
-    float t = 0.5 * (normalize(r.dir()).y() + 1.0);
+    // if the ray hits the sphere generate color in a different way
+    if(hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r))
+        return vec3(1.0, 0.0, 0.0);
+    // if the ray hits places in the background we generate background color
+    float t = 0.58 * (normalize(r.dir()).y() + 1.0);
+    t = 1;
     return vec3(0.5, 1.0, 0.7) * (1-t) + vec3(1.0, 1.0, 1.0) * t;
 }
 
 int main(){
     
     std::fstream File;
-    File.open("blender.ppm",std::ios::out);
+    File.open("sphere.ppm",std::ios::out);
     if(!File){
         File.close();
         std::cout<< "fail to open file\n";
