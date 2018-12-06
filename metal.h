c5f95ebc0e3c761b8e97077da772b60fc6497d75
AlcEccentric <lihongyide@gmail.com>
2018-12-06 22:22:30 +0800

    add fuzzing to metal

--- metal.h
+++ metal.h
@@ -7,9 +7,11 @@ vec3 reflect(const vec3& in, const vec3& n);
 class metal: public material{
 public:
     vec3 albedo;
-    metal(const vec3& a):albedo(a) {};
+    float fuzz;
+    metal(const vec3& a, float f):albedo(a) {if(f < 1) fuzz = f; else fuzz = 1;};
     virtual bool scatter(const ray& r_in, const hit_info& info, vec3& attenuation, ray& scattered) const{
-        scattered = ray(info.p, reflect(r_in.dir(), info.n));
+        
+        scattered = ray(info.p, reflect(normalize(r_in.dir()), info.n) + fuzz * randend_in_unit_sphere());
         attenuation = albedo;
         // only when r_in.dir points in the tangent surface, the r_in will be scatter by the surface
         // "r_in.dir points in"   
